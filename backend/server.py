629cbe5d14a325f26c621379762039c352256e6f
Sisou1905 <ssaadouni@gmail.com>
2026-03-04 01:30:05 +0100

    Update MongoDB client to use TLS
    Enable TLS for MongoDB connection with invalid certs allowed.

--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,7 @@ load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url)
+client = AsyncIOMotorClient(mongo_url, tls=True, tlsAllowInvalidCertificates=True)
 db = client[os.environ['DB_NAME']]
 
 # Resend configuration
