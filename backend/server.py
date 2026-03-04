35af594c5a5d52f5fd44792379defb7d9eb32703
Sisou1905 <ssaadouni@gmail.com>
2026-03-04 22:02:29 +0100

    Increase MongoDB server selection timeout

--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,7 @@ load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
-client = AsyncIOMotorClient(mongo_url, tls=True, tlsAllowInvalidCertificates=True)
+client = AsyncIOMotorClient(mongo_url, serverSelectionTimeoutMS=30000, tls=True, tlsAllowInvalidCertificates=True)
 db = client[os.environ['DB_NAME']]
 
 # Resend configuration
