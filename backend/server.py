01341787a4662cf189c4470c8131b335fd6c9a9e
Sisou1905 <ssaadouni@gmail.com>
2026-03-04 23:01:43 +0100

    Add MongoDB connection to server

--- backend/server.py
+++ backend/server.py
@@ -19,6 +19,7 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
+mongo_url = os.environ['MONGO_URL']
 client = AsyncIOMotorClient(mongo_url, tlsCAFile=certifi.where())
 db = client[os.environ['DB_NAME']]
 # Resend configuration
